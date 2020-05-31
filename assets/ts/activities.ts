20f3277a1b14a50aa50521debdcb084c956e1082
maddoxamei <maddoxamei@gmail.com>
2020-05-30 23:02:55 -0400

    Auto-generated commit

---
+++ assets/ts/activities.ts
@@ -0,0 +1,27 @@
+export const activities = {
+  "Horizons": {
+    "description": "A series begun by the local, non-profit Tallahassee Scientific Society in 2011 to further its mission to promote public interest in science and technology. The series features one presentation per month beginning February and running through April. All presentations are held at TCC’s Center of Innovation in Kleman Plaza at 7 PM; doors open at 6 PM. For memberships, tickets, and more information, go to tallysci.org. Admission is free for all students.",
+    "image": "https://images.unsplash.com/photo-1445966275305-9806327ea2b5?ixlib=rb-0.3.5&amp;q=85&amp;fm=jpg&amp;crop=entropy&amp;cs=srgb&amp;ixid=eyJhcHBfaWQiOjE0NTg5fQ&amp;s=14a02fab1200216368d682ecaa91e540",
+    "photocred": "Anders Jildén"
+  },
+  "Gold Medal Award": {
+    "description": "The Gold Medal Award, established in 2004, is annually bestowed by the Society on a scientist or scholar selected from the Tallahassee community whose career achievements in science or science education and outreach are deemed exemplary. Each fall, the Society hosts the presentation of the award and free public lecture by the awardee, preceded by a reception.",
+    "image": "https://images.unsplash.com/photo-1532094349884-543bc11b234d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+    "photocred": "Hans Reniers"
+  },
+  "Lannutti Awards": {
+    "description": "Professor Joseph Lannutti was one of the most prolific innovators of recent times. He joined the faculty of Florida State University in 1957, and brought distinction to that institution through his extensive contributions to the Department of Physics. Professor Lannutti’s creative talents have also contributed to the excellence of the graduate research programs that exist at Florida State University. The fruits of his labors are to be seen everywhere. The Tallahassee Scientific Society was privileged to welcome Professor Lannutti as a member of the Board of Directors in 1994/95, where his timely direction and advice were very fruitful.",
+    "image": "https://images.unsplash.com/photo-1529854140025-25995121f16f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+    "photocred": "Skye Studios"
+  },
+  "Field Trips": {
+    "description": "The Tallahassee Scientific Society is continuing its series of field trips, which usually are Saturday morning, family friendly, expert-led outings. They are announced first to TSS members by email and later to the general public if spaces remain. Trips in the 2017 and 2018 seasons have focused on karst geology, astronomy, archaeology, dinosaur paleontology, quarry fossil-hunting, and visits to Tall Timbers Research Station and the Hopkins Power Plant. New trips are offered to members by email and advanced reservations are required. Group size is managed for a maximum educational experience, and there are no charges for the trips.",
+    "image": "https://images.unsplash.com/photo-1554741458-08edc3611229?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=967&q=80",
+    "photocred": "Joshua Coleman"
+  },
+  "John Gorrie Lectures": {
+    "description": "With this community-focused series, the Society honors the memory of John Gorrie (1802-1855), the pioneering physician, scientist and inventor who, from his laboratory in Apalachicola, Florida, became the first to invent an artificial means of creating ice. The principles of the machine eventually became the foundation for modern air conditioning. The Gorrie Series features presentations primarily by local scientists hosted by schools and community groups throughout the Big Bend area. The Gorrie Series has included presentations to Gadsden and Jefferson County high school science students and the 2nd Saturday Science Lab held monthly at the Leon County Public Library.",
+    "image": "https://images.unsplash.com/photo-1560264262-fb13f1b9fe74?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+    "photocred": "Arlington Research"
+  }
+};
