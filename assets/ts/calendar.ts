cc52f646fe1b39b8cd620bd9b58c17486ff57499
maddoxamei <maddoxamei@gmail.com>
2020-06-10 12:01:11 -0400

    Auto-generated commit

--- assets/ts/calendar.ts
+++
@@ -1,5 +0,0 @@
-export const calendar = {
-  "Orange": "Members Only",
-  "Blue": "Public, Require Ticket Purchase",
-  "Green": "Public"
-};
--- assets/ts/mission.ts
+++
@@ -1,6 +0,0 @@
-export const mission = ["Organization, promotion and participation in activities involving science and technology",
-            "Provision of a forum in which professional scientists and technologists may review and discuss developments in their respective fields of endeavor",
-            "Provision of assistance in every possible way with the education of school students in scientific subjects",
-            "Provision of a point of reference for information and expertise in the several facets of science and technology",
-            "Recognizing and rewarding outstanding achievements in science and technology in Tallahassee and its environs",
-            "Any other activity consistent with the foregoing and with the purpose of the Corporation, subject to the provisions of Section 501(c)(3) and 107(c)(2) of the Internal Revenue Code and its Regulations as they now exist or as they may hereafter be amended"];
